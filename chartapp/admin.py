836944aae550e452ccf5208d7ffa7667987bf49f
Alan Gibizov <phaggi@gmail.com>
2023-11-28 02:46:23 +0300

    repair imports

--- chartapp/admin.py
+++ chartapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from .models import Temperature
 
 # Register your models here.
-from django.contrib import admin
-from .models import Temperature
+
 
 admin.site.register(Temperature)
