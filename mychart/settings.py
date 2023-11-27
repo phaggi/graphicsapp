83ee35d170625857ae8df42a667a4fa25803727b
Alan Gibizov <phaggi@gmail.com>
2023-11-27 22:31:57 +0300

    change db folder

--- mychart/settings.py
+++ mychart/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mychart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
