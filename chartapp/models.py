e2abb9aa93891b1f0ecabab42989f1f1f3206148
Alan Gibizov <phaggi@gmail.com>
2023-11-27 22:59:44 +0300

    add model Temperature and migrate it

--- chartapp/models.py
+++ chartapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Temperature(models.Model):
+    amount = models.DecimalField(max_digits=11, decimal_places=4)
+    datetime = models.DateTimeField()
