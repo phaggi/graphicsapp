2113164047da512d3e0bc26aded3ce2d55d6534c
Alan Gibizov <phaggi@gmail.com>
2023-11-27 23:14:08 +0300

    add view_func

--- chartapp/views.py
+++ chartapp/views.py
@@ -1,3 +1,14 @@
+import datetime
+
+from django.db.models import Count
 from django.shortcuts import render
+from qsstats import QuerySetStats
+
+from chartapp.models import Temperature
+
 
 # Create your views here.
+def view_func(request):
+    queryset = Temperature.objects.all()
+    values = [[str(e.datetime.time()), int(e.amount)] for e in queryset]
+    return render(request, 'template.html', {'values': values})
